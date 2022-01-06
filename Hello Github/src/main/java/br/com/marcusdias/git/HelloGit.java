d683e8ab9295ca4d8920dae4a0aee5781b477634
1marx <97206116+1marx@users.noreply.github.com>
2022-01-06 18:14:35 -0400

    Adicionei arquivos via UPLOAD

---
+++ Hello Github/src/main/java/br/com/marcusdias/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.marcusdias.git;
+
+/**
+ * @author engma
+ * Exemplo de integração de Eclipse com GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
